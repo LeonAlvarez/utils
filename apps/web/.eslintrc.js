30781a3ae85fe4a9f8f2d2a57d8053bd2c934183
LeonAlvarez <leon@leon-design.es>
2024-10-23 11:10:26 +0200

    fix eslint config

--- apps/web/.eslintrc.js
+++
@@ -1,9 +0,0 @@
-/** @type {import("eslint").Linter.Config} */
-export default {
-  root: true,
-  extends: ['@ladc.dev/eslint-config/next.js'],
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    project: true,
-  },
-};
--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,9 @@
 import eslintRecommended from '@eslint/js';
 import prettierConfig from 'eslint-plugin-prettier/recommended';
 import react from 'eslint-plugin-react';
+import tsPlugin from '@typescript-eslint/eslint-plugin';
+import parser from '@typescript-eslint/parser';
+import globals from 'globals';
 
 export default [
   eslintRecommended.configs.recommended,
@@ -8,14 +11,17 @@ export default [
   {
     files: ['**/*.{js,jsx,ts,tsx}'],
     languageOptions: {
+      parser,
       ecmaVersion: 2021,
       sourceType: 'module',
       globals: {
-        //
+        ...globals.browser,
+        ...globals.node,
       },
     },
     plugins: {
       react,
+      tsPlugin,
     },
     rules: {
       // Define your custom rules here
