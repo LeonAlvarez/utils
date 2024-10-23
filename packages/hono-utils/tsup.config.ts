7f5edc93b6bbc3807aee84d2fbbdda04ba4244a4
LeonAlvarez <leon@leon-design.es>
2024-10-23 11:53:07 +0200

    update library exports

--- packages/hono-utils/tsup.config.ts
+++ packages/hono-utils/tsup.config.ts
@@ -2,11 +2,12 @@ import { defineConfig } from 'tsup'
 
 export default defineConfig({
   entry: ['src/index.ts'],
+  target: 'es2017',
+  format: ['esm', 'cjs'],
   splitting: false,
-  sourcemap: false,
+  sourcemap: true,
   clean: true,
   dts: true,
   treeshake: true,
   shims: true,
-  format: ['esm', 'cjs']
 })
